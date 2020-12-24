fd33adfe2d8df97573d32b9760eaf0980046b589
= <garvitgalgat@gmail.com>
2020-12-25 00:38:35 +0530

    home page without button + lorem

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Home.css'
 
+
 export default function Home() {
     return (
-        <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam labore non voluptatem? Optio, iste illo maiores exercitationem autem adipisci ab facere debitis magni, voluptatem explicabo, reiciendis ex aliquam? Sed animi blanditiis suscipit quos, exercitationem nam minus? Quae, ex ipsa provident optio ipsam facere soluta, vel exercitationem tempore iure quidem quam officia facilis quis ipsum magni. Perferendis, impedit. Mollitia, dicta?</p>
+        <div className="home-content">
+            <div className="left">
+                <div className="welcome">Welcome</div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam labore non voluptatem? Optio, iste illo maiores exercitationem autem adipisci ab facere debitis magni, voluptatem explicabo, reiciendis ex aliquam? Sed animi blanditiis suscipit quos, exercitationem nam minus? Quae, ex ipsa provident optio ipsam facere soluta, vel exercitationem tempore iure quidem quam officia facilis quis ipsum magni. Perferendis, impedit. Mollitia, dicta?</p>
+            </div>
+
+            <div className="right">
+                <button>DOWNLOAD</button>
+            </div>
+
         </div>
     )
 }
