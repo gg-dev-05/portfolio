1b8438c89d21931f06477ca06796e84f89af2e31
= <garvitgalgat@gmail.com>
2020-12-25 19:28:56 +0530

    about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,10 +3,50 @@ import './About.css'
 export default function About() {
     return (
         <div className="about-me page">
-            <div className="about">About ME</div>
 
-            <div className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ducimus rem omnis mollitia voluptate nulla, id voluptatum, cupiditate tempore aspernatur quod deserunt rerum itaque. Unde soluta amet blanditiis quidem quaerat, doloribus ad accusamus alias aperiam sit suscipit libero expedita, cumque similique nesciunt voluptates, illo iure beatae quod. Unde corrupti, ad, nulla dolorem ex, molestias aspernatur sint nemo velit aliquid pariatur. Eius culpa dolore harum reprehenderit modi veniam minus? Iure corporis eligendi, ex voluptas id sequi cumque illum labore voluptates cupiditate aliquid dolore nostrum at modi officiis voluptate aliquam, accusamus commodi eius ab facilis! Itaque, at atque delectus expedita voluptas quidem.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nam obcaecati quisquam laborum alias cupiditate error labore, deleniti consequatur nobis ipsam quam distinctio aliquam adipisci molestias sint, unde numquam, delectus laudantium consequuntur atque itaque blanditiis. Ipsum voluptatum hic pariatur, amet a voluptas dicta ipsam ab nulla eos beatae rem quae ea vel porro fugit quo perferendis, velit aliquid enim? Earum, ea molestiae vitae, et cumque voluptatum tenetur ducimus accusantium officia ipsa doloremque repellendus placeat repudiandae facilis sed minima vel ut eius accusamus odio nobis magni porro ex architecto. Nam corporis in id perferendis quia, sequi quibusdam fugit ab temporibus maiores!</div>
+            <span className="heading">I am <span className="change-on-hover">Garvit Galgat</span>,</span>
+            <span className="content"> a 2nd yr. B.Tech CSE student of <span className="change-on-hover"> Indian Institute of Technology Indore</span></span>.
+
+            <div>I am a budding <span className="change-on-hover">Full Stack Developer</span> learning about different tech stacks and making various <span className="change-on-hover">projects</span> and
+            participating in various <span className="change-on-hover">hackathons</span>.
+            </div>
+
+            <div className="stack">
+                <div className="frontend">
+                    <h4>Frontend Technologies </h4>
+                    <div className="items">
+                        <i class="fab fa-html5 fa-3x" title="HTML5"></i>
+                        <i class="fab fa-css3 fa-3x" title="CSS3"></i>
+                        <i class="fab fa-js fa-3x" title="Javascript"></i>
+                        <i class="fab fa-sass fa-3x" title="SASS"></i>
+                        <i class="fab fa-bootstrap fa-3x" title="Bootstrap"></i>
+                        <i class="fab fa-react fa-3x" title="React"></i>
+                    </div>
+
+                </div>
+
+                <div className="backend">
+                    <h4>Backend Technologies</h4>
+                    <div className="items">
+                        <i class="fab fa-node fa-3x" title="NodeJs"></i>
+                        <i class="fab fa-python fa-3x" title="Python"></i>
+                        <img src="https://www.vectorlogo.zone/logos/expressjs/expressjs-ar21.svg" alt="express" title="expressJs" />
+                        <img src="https://www.vectorlogo.zone/logos/pocoo_flask/pocoo_flask-icon.svg" alt="flask" title="Flask" />
+                        <img src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg" alt="heroku" title="Heroku" />
+                    </div>
+                </div>
+
+                <div className="database">
+                    <h4>Databases</h4>
+                    <div className="items">
+                        <img src="https://www.vectorlogo.zone/logos/mysql/mysql-icon.svg" alt="mysql" title="mysql" />
+                        <img src="https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg" alt="mongo" title="mongo" />
+                    </div>
+                </div>
+            </div>
+
+
+
         </div>
     )
 }
