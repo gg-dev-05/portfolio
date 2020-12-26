4cfabe7a4e632cdb4f58af1057859bb60022a80c
= <garvitgalgat@gmail.com>
2020-12-26 08:21:53 +0530

    home content added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,14 +7,14 @@ import "react-awesome-button/dist/styles.css";
 export default function Home() {
     return (
         <div className="home-content page">
-            <div className="left">
-                <div className="welcome">Welcome</div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam labore non voluptatem? Optio, iste illo maiores exercitationem autem adipisci ab facere debitis magni, voluptatem explicabo, reiciendis ex aliquam? Sed animi blanditiis suscipit quos, exercitationem nam minus? Quae, ex ipsa provident optio ipsam facere soluta, vel exercitationem tempore iure quidem quam officia facilis quis ipsum magni. Perferendis, impedit. Mollitia, dicta?</p>
+            <div className="content">
+                Hi, Welcome to my portfolio. <br /> I am Garvit Galgat, a Full-Stack Developer.
+
             </div>
 
-            <div className="right">
+            <div className="resume">
                 <a href="https://drive.google.com/file/d/1n2q5Pe9g5XD98oaflmF1lyuuf_CK4TB-/view" target="_blank" rel="noreferrer">
-                    <AwesomeButton type="primary"><i className="fas fa-download"></i>Download Resume</AwesomeButton>
+                    <AwesomeButton type="primary"><i className="fas fa-download"></i>View My Resume</AwesomeButton>
 
                 </a>
             </div>
