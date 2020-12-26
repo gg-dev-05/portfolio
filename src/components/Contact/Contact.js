afcd44f15e0bddc365e749413c67b1e0d0ef5f15
= <garvitgalgat@gmail.com>
2020-12-26 19:25:34 +0530

    form validations + mail send

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useEffect } from "react";
+import React, { useState } from 'react'
+// import { useEffect } from "react";
 
 import { AwesomeButton } from "react-awesome-button";
 import "react-awesome-button/dist/styles.css";
@@ -10,40 +10,62 @@ import './Contact.css'
 
 export default function Contact() {
 
-    useEffect(() => {
-        console.log("I am ready")
-    })
+    var [buttonState, setButtonState] = useState(false)
 
-    function validateEmail(email) {
+    var validateEmail = () => {
         var re = /\S+@\S+\.\S+/;
-        return re.test(email);
+        var valid = re.test(document.getElementById("email-textarea").value);
+        if (!valid) {
+            document.getElementById("email-textarea").value = ""
+            document.getElementById("email-textarea").focus()
+        }
     }
 
     var handleChange = (elem) => {
-        var id = elem.target.id
-        var value = document.getElementById(id).value
+        // var id = elem.target.id
+        // var value = document.getElementById(id).value
 
-        if (id === 'email-textarea') {
-            console.log(validateEmail(value))
-        }
+        // if (id === 'email-textarea') {
+        //     // console.log(validateEmail(value))
+        // }
 
     }
 
     var validateData = () => {
         var ids = ['name', 'email', 'subject', 'message']
         var data = []
+        var allOk = true;
         ids.forEach((id) => {
             var value = document.getElementById(id + "-textarea").value
-            data.push(value)
+            if (allOk) {
+                // console.log(value)
+                if (value === '') {
+                    document.getElementById(id + '-textarea').focus();
+                    allOk = false;
+                }
+                else {
+                    data.push(value)
+
+                }
+            }
         })
-        var json = {
-            "name": data[0],
-            "from": data[1],
-            "subject": data[2],
-            "message": data[3]
+        if (allOk) {
+            var json = {
+                "name": data[0],
+                "from": data[1],
+                "subject": data[2],
+                "message": data[3]
+            }
+            // console.log(json)
+            // console.log(document.getElementsByClassName('send-button'))
+            setButtonState(!buttonState)
+            axios.post('https://mail-sender-g.herokuapp.com/send', data = json)
+                .then((r) => {
+                    setButtonState(!setButtonState)
+                })
         }
-        axios.post('https://mail-sender-g.herokuapp.com/send', data = json)
-            .then((r) => console.log(r))
+
+
     }
 
     var handleClick = () => {
@@ -60,7 +82,7 @@ export default function Contact() {
                 </div>
 
                 <div className="email">
-                    <textarea placeholder="Email" id="email-textarea" name="email" onChange={handleChange}></textarea>
+                    <textarea placeholder="Email" id="email-textarea" name="email" onChange={handleChange} onBlur={validateEmail}></textarea>
                 </div>
 
                 <div className="subject">
@@ -71,7 +93,7 @@ export default function Contact() {
                     <textarea placeholder="Message" id="message-textarea" name="message" onChange={handleChange}></textarea>
                 </div>
                 <div className="send-button" onClick={handleClick}>
-                    <AwesomeButton type="primary">Send</AwesomeButton>
+                    <AwesomeButton id="awesome-button" type="primary" disabled={buttonState}>Send</AwesomeButton>
 
                 </div>
             </div>
