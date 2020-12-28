2b21f4aae7ce205189291dc3abde03fa865bd414
= <garvitgalgat@gmail.com>
2020-12-28 06:09:22 +0530

    card component : hover + tags showing

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,7 +6,10 @@ import Card from './Card/Card'
 export default function Projects() {
     return (
         <div className="my-work page">
-            <Card />
+            <Card img1="https://picsum.photos/id/237/200/300" img2="https://picsum.photos/id/232/200/300" />
+            <Card img1="https://picsum.photos/id/217/200/300" img2="https://picsum.photos/id/230/200/300" />
+            <Card img1="https://picsum.photos/id/234/200/300" img2="https://picsum.photos/id/111/200/300" />
+
 
         </div>
     )
