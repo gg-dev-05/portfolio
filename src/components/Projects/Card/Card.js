dfffd878ff51272173f9f0273113f99e06eb7ce1
= <garvitgalgat@gmail.com>
2020-12-29 20:54:49 +0530

    Card Component created

--- src/components/Projects/Card/Card.js
+++ src/components/Projects/Card/Card.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import './Card.css'
+
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
+
 export default function Card(props) {
 
 
+    var tags = [];
+
+    for (let i = 0; i < props.tags.length; i++) {
+        var delay = Number(i) * 0.07;
+        delay = delay + "s"
+        tags.push(
+            <span key={i} className="bouncy" style={{ animationDelay: delay }}>{props.tags[i]}</span>
+        )
+    }
     return (
+
 
         <div className="Card">
             <img className="back" src={props.img1} alt="back" />
             <div className="front visible-on-hover">
                 <div className="container">
                     <div className="tags">
-                        <span>Item1</span>
-                        <span>Item1</span>
-                        <span>Item1</span>
-                        <span>Item1</span>
-                        <span>Item1</span>
-
-
+                        {tags}
                     </div>
+                    <a href={props.link} target="_blank" rel="noreferrer"><AwesomeButton type="primary">Know More</AwesomeButton></a>
 
                 </div>
                 <img className="" src={props.img2} alt="front" />
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,9 +6,15 @@ import Card from './Card/Card'
 export default function Projects() {
     return (
         <div className="my-work page">
-            <Card img1="https://picsum.photos/id/237/200/300" img2="https://picsum.photos/id/232/200/300" />
-            <Card img1="https://picsum.photos/id/217/200/300" img2="https://picsum.photos/id/230/200/300" />
-            <Card img1="https://picsum.photos/id/234/200/300" img2="https://picsum.photos/id/111/200/300" />
+            <Card img1="https://picsum.photos/id/237/200/300" img2="https://picsum.photos/id/232/200/300"
+                tags={["HTML", "CSS", "JS", "JQuery", "Heroku"]} link="https://github.com/gg-dev-05/clubsiiti"
+            />
+            <Card img1="https://picsum.photos/id/217/200/300" img2="https://picsum.photos/id/230/200/300"
+                tags={["Heroku", "ReactJs", "MYSQL", "Flask"]} link="https://github.com/gg-dev-05/secure-blink"
+            />
+            <Card img1="https://picsum.photos/id/234/200/300" img2="https://picsum.photos/id/111/200/300"
+                tags={["JSX", "GCP", "Mongod", "JQuery", "HTML"]} link="https://github.com/gg-dev-05/secure-blink"
+            />
 
 
         </div>
